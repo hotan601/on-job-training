8f773c4f87c556e83aa6c9f781f8a450c5a2a92e
ADMIN-PC\Tan <hovantan601@gmail.com>
2024-10-12 14:16:25 +0700

    lombok

--- src/main/java/fin3/tanhv4/dto/TestDTO.java
+++ src/main/java/fin3/tanhv4/dto/TestDTO.java
@@ -1,8 +1,20 @@
 package fin3.tanhv4.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class TestDTO {
 	private String testID;
 	private String testName;
 	private int testAge;
 	private String testAddress;
+
+
 }
